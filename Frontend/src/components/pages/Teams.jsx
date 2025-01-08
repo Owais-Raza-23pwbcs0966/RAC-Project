e69d2b31caee662ce329702f768b9df5aa15d2c8
Owais-Raza-23pwbcs0966 <23pwbcs0966@uetpeshawar.edu.pk>
2025-01-08 18:09:11 +0500

    Update Teams.jsx

--- Frontend/src/components/pages/Teams.jsx
+++ Frontend/src/components/pages/Teams.jsx
@@ -15,7 +15,7 @@ export const Teams = () => {
   // useEffect hook to fetch data when the component mounts
   useEffect(() => {
     axios
-      .get("http://rac-project.vercel.app/teamData") // API call to fetch team data
+      .get("https://rac-project.vercel.app/teamData") // API call to fetch team data
       .then((res) => {
         setTeamData(res.data); // Save fetched data to state
         setLoading(false); // Set loading to false once data is fetched
