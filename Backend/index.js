00c4a991286600308eec8ff600dccc26bbdf3958
Owais-Raza-23pwbcs0966 <23pwbcs0966@uetpeshawar.edu.pk>
2025-01-08 19:14:28 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,11 +15,19 @@ const PORT = 5000;
 
 // Middleware setup
 app.use(bodyParser.json());  // Parse incoming request bodies as JSON
-app.use(cors({
-    origin:['https://rac-project-frontend.vercel.app'],
-    methods:['POST','GET'],
-    credentials:true
-}));  // Enable Cross-Origin Resource Sharing (CORS) for all routes
+// app.use(cors({
+//     origin:['https://rac-project-frontend.vercel.app'],
+//     methods:['POST','GET'],
+//     credentials:true
+// }));  // Enable Cross-Origin Resource Sharing (CORS) for all routes
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://rac-project-frontend.vercel.app'); // Specific origin
+    res.header('Access-Control-Allow-Methods', 'GET,POST'); // Allowed methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
+    res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
+    next();
+});
 
 // MongoDB Connection
 const mongo_URI = process.env.MONGO_URI;  // Get MongoDB URI from environment variable
